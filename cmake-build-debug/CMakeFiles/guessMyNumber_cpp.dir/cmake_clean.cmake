1593fbc5547eb918f680f5a3b9f89cb879c6f250
jimin huh <jiminhuh95@gmail.com>
2020-07-05 15:05:24 -0500

    initializing git]

---
+++ cmake-build-debug/CMakeFiles/guessMyNumber_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guessMyNumber_cpp.dir/main.cpp.obj"
+  "guessMyNumber_cpp.exe"
+  "guessMyNumber_cpp.exe.manifest"
+  "guessMyNumber_cpp.pdb"
+  "libguessMyNumber_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/guessMyNumber_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
